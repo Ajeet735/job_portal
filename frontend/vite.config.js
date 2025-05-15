c8e0053d8a4010848e2549ee55d24db80a089d8b
AjeetKumar <ajeetkumar.dev735@gmail.com>
2025-05-15 15:55:10 +0530

    Fixed vite config and plugin setup for Vercel deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    open: true, // opens browser
+    open: true,
   },
   plugins: [react()],
   base: process.env.NODE_ENV === 'production' ? '/job-portal/' : '/',
-})
+});
